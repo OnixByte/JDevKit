89837f78f33a8db1ad5da4e854db8c7b019768c5
Zihlu Wang <zihlu.wang@outlook.com>
2023-07-30 00:34:35 +0800

    docs(global): optimised docs

---
+++ devkit-core/src/main/java/cn/org/codecrafters/devkit/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023 CodeCraftersCN.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * JDevKit package, please see documents for more details.
+ * <p>
+ * You may check the details of JDevKit at <a
+ * href="https://codecrafters.org.cn/JDevKit"> codecrafters.org.cn </a>
+ *
+ * @author Zihlu Wang
+ * @since 1.0.0
+ */
+package cn.org.codecrafters.devkit;
